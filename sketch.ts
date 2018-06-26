ec69a7ee3a9617884b49bb209697424121c8e4cb
Fox <7926053+SrZorro@users.noreply.github.com>
2018-06-26 10:56:30 +0200

    Sketch WIP

--- sketch.ts
+++ sketch.ts
@@ -1,30 +1,19 @@
 import AminoBot from "./src";
 
-const bot = new AminoBot("**REDACTED**", "**REDACTED**", "**REDACTED**");
-bot.on("login", (comms) => {
-    console.log(comms);
-
-    console.log(comms.names);
-
-    // Listen to communities where his name contains /regex/
-    comms.on(/programming/, (community) => {
-        // ToDo...
-    });
-
-    // Listen to all other communities
-    for (const communityName of comms.names) {
-        if (!communityName.includes("programming"))
-            comms.on(communityName, (community) => {
-                // Listen all communities that don't contain "programming"
-                // ToDo...
-            });
-    }
-});
-
-bot.on("loginFailed", (err) => {
-    console.error(err);
-});
+const bot = new AminoBot(process.env.AMINO_EMAIL as string, process.env.AMINO_PWD as string, process.env.AMINO_DEVICEID as string);
 
 (async () => {
-    await bot.init();
+    // await bot.init();
+    try {
+        const communities = await bot.onLogin();
+
+        for (const comm of communities.communityList) {
+            communities.on(comm.endpoint, (community) => {
+                console.log(community);
+            })
+        }
+    } catch (err) {
+        console.log("Error! Lets think about this later...");
+        console.error(err);
+    }
 })();
